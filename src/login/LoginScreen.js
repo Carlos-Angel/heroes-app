def7dc9643e7db3ed5f41f94fa3a225bf6bb8248
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-11 04:56:52 -0500

    feat: estructura del componente LoginScreen

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export default function LoginScreen() {
-  return <div>LoginScreen</div>;
+export default function LoginScreen({ history }) {
+  const handleClick = () => {
+    history.replace('/');
+  };
+
+  return (
+    <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr />
+      <button className='btn btn-primary' onClick={handleClick}>
+        Login
+      </button>
+    </div>
+  );
 }
