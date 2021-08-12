4611321cbf89f7ec2672af03342fbc027e1031d7
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-12 10:20:22 -0500

    fix: typo :heroId

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -14,7 +14,7 @@ export default function DashboardRoutes() {
         <Switch>
           <Route exact path='/marvel' component={MarvelScreen} />
           <Route exact path='/dc' component={DcScreen} />
-          <Route exact path='/hero/heroId' component={HeroScreen} />
+          <Route exact path='/hero/:heroId' component={HeroScreen} />
 
           <Redirect to='/marvel' />
         </Switch>
