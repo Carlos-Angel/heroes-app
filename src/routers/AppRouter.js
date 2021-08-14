357f014e74186e5da1e0e0b19bb937c752666d2c
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-13 21:26:42 -0500

    feat: configuración para rutas privadas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import LoginScreen from '../login/LoginScreen';
 import DashboardRoutes from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
 
 export default function AppRouter() {
+  const { user } = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <div>
         <Switch>
           <Route exact path='/login' component={LoginScreen} />
-          <Route path='/' component={DashboardRoutes} />
+          <PrivateRoute
+            path='/'
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </BrowserRouter>
