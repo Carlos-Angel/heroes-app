af26def8681e3a605f2b219cf66a70393c1e0b77
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-08-09 19:21:13 -0500

    refactor: cambiar estructura de archivos

--- src/helpers/heroImages.js
+++
@@ -1,7 +0,0 @@
-let heroImages = () => ({ default: '' });
-
-try {
-  heroImages = require.context('assets/heroes', true);
-} catch (error) {}
-
-export const loadImage = (image) => heroImages(`./${image}`).default;
--- src/selectors/getHeroesByName.js
+++
@@ -1,8 +0,0 @@
-import { heroes } from 'data/heroes';
-
-export const getHeroesByName = (search = '') => {
-  const name = search.toLocaleLowerCase();
-  return name !== ''
-    ? heroes.filter((hero) => hero.superhero.toLocaleLowerCase().includes(name))
-    : [];
-};
